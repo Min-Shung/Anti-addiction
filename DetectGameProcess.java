ee70b2b146b0c639eb3ba57f7ec87a182255c5cd
Pei-0923 <151004666+Pei-0923@users.noreply.github.com>
2025-05-22 14:54:38 +0800

    遊戲清單

---
+++ DetectGameProcess.java
@@ -0,0 +1,24 @@
+//遊戲清單
+public class DetectGameProcess {
+    public static final String[] gameExecutables = {
+        "chrome.exe",                      //google 測試用
+
+        "javaw.exe",                       // Minecraft
+        "VALORANT-Win64-Shipping.exe",    // Valorant
+        "LeagueClient.exe",                // League of Legends
+        "cs2.exe",                        // CS2 (Counter-Strike 2)
+        "GTA5.exe",                      // GTA V
+        "r5apex.exe",                    // Apex Legends
+        "FortniteClient-Win64-Shipping.exe", // Fortnite
+        "YuanShen.exe",                  // 原神 (Genshin Impact) 中國版
+        "GenshinImpact.exe",             // 原神 (Genshin Impact) 全球版
+        "StarRail.exe",                  // 崩壞：星穹鐵道 (Honkai: Star Rail)
+        "RobloxPlayerBeta.exe",          // Roblox
+        "TslGame.exe",                   // PUBG (PlayerUnknown's Battlegrounds)
+        "eldenring.exe",                 // Elden Ring
+        "Cyberpunk2077.exe",             // Cyberpunk 2077
+        "Overwatch.exe",                 // Overwatch 2
+        "TS4_x64.exe",                   // The Sims 4
+        "Stardew Valley.exe"             // Stardew Valley
+    };
+}
