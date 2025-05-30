185314ab842349f94c842abc7f6eb10ec434c9ba
Pei-0923 <151004666+Pei-0923@users.noreply.github.com>
2025-05-30 15:22:59 +0800

    關所有有開的遊戲

--- KillGame.java
+++ KillGame.java
@@ -1,19 +1,26 @@
 import java.io.IOException;
+import java.util.List;
+
 public class KillGame {
     
-    public static void killRunningGame() {
-        String runningGame = GameDetection.getRunningGame(DetectGameProcess.gameExecutables);
+    public static void killRunningGames() {
+        List<String> runningGames = GameDetection.getRunningGames(DetectGameProcess.gameExecutables);
 
-        if (runningGame != null) {
-            try {
-                ProcessBuilder builder = new ProcessBuilder("taskkill", "/F", "/IM", runningGame);
-                builder.inheritIO();
-                builder.start();
-                System.out.println("已嘗試關閉：" + runningGame);
-            } catch (IOException e) {
-                System.out.println("關閉失敗");
-                e.printStackTrace();
+        if (!runningGames.isEmpty()) {
+            for (String runningGame : runningGames) {
+                try {
+                    ProcessBuilder builder = new ProcessBuilder("taskkill", "/F", "/IM", runningGame);
+                    builder.inheritIO();
+                    builder.start();
+                    System.out.println("已嘗試關閉：" + runningGame);
+                } catch (IOException e) {
+                    System.out.println("關閉 " + runningGame + " 失敗");
+                    e.printStackTrace();
+                }
             }
+        } else {
+            System.out.println("沒有偵測到正在執行的遊戲");
         }
     }
+    
 }
