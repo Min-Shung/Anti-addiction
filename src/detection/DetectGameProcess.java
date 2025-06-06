32988e403fd08019fb4fb0c38bfeac7e723c59fb
Pei-0923 <151004666+Pei-0923@users.noreply.github.com>
2025-06-07 01:25:45 +0800

    加長遊戲名單

--- src/detection/DetectGameProcess.java
+++ src/detection/DetectGameProcess.java
@@ -1,26 +1,88 @@
-//遊戲清單
+// 遊戲清單
 package detection;
+
 public class DetectGameProcess {
     public static final String[] gameExecutables = {
-        "chrome.exe",                      //google 測試用
+        "chrome.exe",                      // Google 測試用
 
-        "javaw.exe",                       // Minecraft
-        "VALORANT-Win64-Shipping.exe",    // Valorant
-        "LeagueClient.exe",                // League of Legends
-        "cs2.exe",                        // CS2 (Counter-Strike 2)
-        "GTA5.exe",                      // GTA V
-        "r5apex.exe",                    // Apex Legends
-        "FortniteClient-Win64-Shipping.exe", // Fortnite
-        "YuanShen.exe",                  // 原神 (Genshin Impact) 中國版
-        "GenshinImpact.exe",             // 原神 (Genshin Impact) 全球版
-        "StarRail.exe",                  // 崩壞：星穹鐵道 (Honkai: Star Rail)
-        "RobloxPlayerBeta.exe",          // Roblox
-        "TslGame.exe",                   // PUBG (PlayerUnknown's Battlegrounds)
-        "eldenring.exe",                 // Elden Ring
-        "Cyberpunk2077.exe",             // Cyberpunk 2077
-        "Overwatch.exe",                 // Overwatch 2
-        "TS4_x64.exe",                   // The Sims 4
-        "Stardew Valley.exe",             // Stardew Valley
-        "steam.exe"                        // Steam 平台啟動器
+        // MOBA
+        "LeagueClient.exe",               // 英雄聯盟
+        "Dota2.exe",                      // Dota 2
+        "Smite.exe",                      // Smite
+        "HeroesOfTheStorm.exe",           // 暴雪英霸
+
+        // FPS / 射擊遊戲
+        "Counter-Strike 2.exe",           // CS2
+        "VALORANT-Win64-Shipping.exe",    // VALORANT
+        "Apex.exe",                        // Apex Legends
+        "Overwatch 2.exe",                 // Overwatch 2
+        "RainbowSix.exe",                 // Rainbow Six Siege
+        "CallOfDutyMW2.exe",              // 決勝時刻：現代戰爭II
+        "CallOfDutyWarzone.exe",          // 決勝時刻：Warzone
+        "Battlefield2042.exe",            // 戰地風雲2042
+        "EscapeFromTarkov.exe",           // 逃離塔科夫
+        "PUBG.exe",                        // PUBG
+
+        // MMORPG
+        "WorldOfWarcraft.exe",            // 魔獸世界
+        "FFXIV.exe",                       // 最終幻想14
+        "BlackDesert64.exe",              // 黑色沙漠
+        "LostArk.exe",                     // 失落的方舟
+
+        // RPG / 動作冒險
+        "MonsterHunterWilds.exe",         // 魔物獵人 荒野
+        "BlackMythWukong.exe",            // 黑神話：悟空
+        "GenshinImpact.exe",              // 原神 國際版
+        "YuanShen.exe",                   // 原神 中國版
+        "Diablo4.exe",                     // 暗黑破壞神 IV
+        "TheWitcher3.exe",                // 巫師3
+        "DarkSoulsIII.exe",               // 黑暗靈魂3
+        "KingdomCome.exe",                // 天國：拯救
+        "SkyrimSE.exe",                   // 上古卷軸V：天際
+        "Cyberpunk2077.exe",              // 電馭叛客2077
+        "RedDeadRedemption2.exe",         // 碧血狂殺2
+        "EldenRing.exe",                  // 艾爾登法環
+        "HogwartsLegacy.exe",             // 霍格華茲的傳承
+        "JustCause4.exe",                 // 正當防衛4
+        "WatchDogsLegion.exe",            // 看門狗：自由軍團
+        "Starfield.exe",                  // 星空
+
+        // 沙盒 / 生存 / 建造
+        "Minecraft.exe",                  // Minecraft
+        "Terraria.exe",                   // 泰拉瑞亞
+        "Valheim.exe",                    // 瓦爾海姆
+        "Factorio.exe",                   // 當個創世神
+        "CitiesSkylines.exe",             // 都市：天際線
+        "PlanetZoo.exe",                  // 星球動物園
+        "StardewValley.exe",              // 星露谷物語
+        "Satisfactory.exe",               // 滿意工廠
+        "Phasmophobia.exe",               // 恐鬼症
+        "DeadByDaylight-Win64-Shipping.exe", // 黎明死線
+        "ProjectZomboid.exe",             // 殭屍毀滅工程
+
+        // 恐怖 / 劇情
+        "ResidentEvil4Remake.exe",        // 惡靈古堡4 重製版
+        "IdentityV.exe",                  // 第五人格
+
+        // 體育 / 競速
+        "FIFA23.exe",                     // FIFA 23
+        "NBA2K23.exe",                    // NBA 2K23
+        "ForzaHorizon5.exe",              // 極限競速：地平線5
+        "AssettoCorsa.exe",               // 神力科莎
+        "RocketLeague.exe",               // 火箭聯盟
+
+        // 派對 / 社交
+        "FallGuys_client_game.exe",       // 糖豆人
+        "AmongUs.exe",                    // Among Us
+        "BeatSaber.exe",                  // Beat Saber
+        "Osu!.exe",                        // Osu!
+
+        // 啟動器
+        "steam.exe",                       // Steam
+        "EpicGamesLauncher.exe",          // Epic Games
+        "Battle.net.exe",                 // Battle.net
+        "riotclientservices.exe",         // Riot 啟動器
+        "UbisoftConnect.exe",             // Ubisoft Connect
+        "Origin.exe"                       // Origin
     };
 }
